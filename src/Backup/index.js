b7fbc0ece2b397948b2dd710923f4efbd03e5af6
H. Elturkey <13208801+helturkey@users.noreply.github.com>
2019-11-30 12:41:31 +0200

    Add files via upload

--- src/Backup/index.js
+++ src/Backup/index.js
@@ -12,48 +12,90 @@ const fs = require('fs')
 const path = require('path')
 const { promisify } = require('util')
 const archiver = require('archiver-promise')
-
-const Helpers = use('Helpers')
-const Config = use('Config')
-const backup = Config.get('backup')
-
+const Farbe = require('kleur')
 const readdir = promisify(fs.readdir)
 const stat = promisify(fs.stat)
 const unlink = promisify(fs.unlink)
 
+const Helpers = use('Helpers')
+const Config = use('Config')
+const backup = Config.get('backup')
+const log = console.log
 
-async function runBackup() {
-    const fileName = await backupFile()
-    const _resources = Helpers.publicPath()
-    console.log('GatherMan is walking your directories...')
-    const resources = await walk(_resources)
-    console.log('Hi, I have finished walking, now I am compressing...')
+
+async function backupFactory(options) {
+
+    if(options.cloudOnly && options.localOnly){
+     log(Farbe.red('seriously!, it is not an acceptable argument!'))
+        return
+    }
+
+    if (!Array.isArray(backup.include) || backup.include.length == 0) {
+        log(Farbe.red('include, only array is allowed.'))
+        return
+    }
+
+    const fileName = await backupFileName()
+
+    log(Farbe.green('GatherMan is walking your directories...'))
+
+    let resources = []
+
+    for (let d of backup.include) {
+        if (!d.includes(Helpers.appRoot())) {
+            log(Farbe.red('it is not a good practice to walk in not-app-root directories.'))
+            break
+        }
+        resources.push(...await walk(d))
+    }
+
+    if (!resources.length) {
+        log(Farbe.red('you can not backup nothing, can you!'))
+        return
+    }
+
+    log(Farbe.green('Hi, I have finished walking, now I am compressing...'))
+
     await compress(fileName, resources)
-    if (backup.destination.disks.includes('s3')) {
+
+    if (backup.destination.disks.includes('s3') && options.localOnly !== true) {
         await s3(fileName)
     }
-    if(backup.method === 'tar')
+
+    if (backup.method === 'tar')
         await unlink(Helpers.appRoot('tar'))
-    else if(backup.method === 'zip')
+    else if (backup.method === 'zip')
         await unlink(Helpers.appRoot('zip'))
+
+    if (options.cloudOnly === true && backup.destination.disks.includes('s3')) {
+        await unlink(Helpers.tmpPath('backup/') + fileName)
+    } else if (options.cloudOnly === true && !backup.destination.disks.includes('s3')) {
+        log(Farbe.red('confusing, trying to delete local backup file without uploading it to s3.'))
+    }
 }
 
-async function backupFile() {
+async function backupFileName() {
     const now = new Date().toISOString().slice(0, 19)
-
     let extension = backup.method
     if (backup.gzip === true && extension === 'tar') {
         extension = extension + '.gz'
     }
-
     return backup.destination.filename_prefix + now + '.' + extension
 }
 
 async function walk(_resources) {
+    // exclude certain directories from backup.
+    if (backup.exclude.includes(_resources)) {
+        return []
+    }
+
     let files = await readdir(_resources)
+
+    // return path only in case of empty directory.
     if (files.length === 0) {
         return _resources
     }
+
     files = await Promise.all(files.map(async file => {
         const filePath = path.join(_resources, file)
         const stats = await stat(filePath)
@@ -63,7 +105,10 @@ async function walk(_resources) {
             return filePath
         }
     }))
-    return files.reduce((all, folderContents) => all.concat(folderContents), [])
+
+    let result = files.reduce((all, folderContents) => all.concat(folderContents), [])
+    // remove empty values from result array.
+    return result.filter(Boolean)
 }
 
 async function compress(fileName, resources) {
@@ -76,14 +121,14 @@ async function compress(fileName, resources) {
 
     const output = fs.createWriteStream(Helpers.tmpPath('backup/') + fileName)
 
-    output.on('close', function() {
-        console.log("total size is " + humanSize(archive.pointer()))
-        console.log('Hola, Backup is done!')
-    });
+    output.on('close', () => {
+        log(Farbe.cyan("file size is: " + humanSize(archive.pointer())))
+        log(Farbe.cyan('congratulation, compressing has been done!'))
+    })
 
     output.on('end', function() {
-        console.log('Data has been drained')
-    });
+        log(Farbe.red('Data has been drained'))
+    })
 
     archive.on('warning', function(err) {
         if (err.code === 'ENOENT') {
@@ -91,13 +136,14 @@ async function compress(fileName, resources) {
         } else {
             throw err
         }
-    });
+    })
 
     archive.on('error', function(err) {
         throw err
-    });
+    })
 
     archive.pipe(output)
+
     for (let _file of resources) {
         const stats = await stat(_file)
         if (stats.isDirectory()) {
@@ -112,22 +158,23 @@ async function compress(fileName, resources) {
 async function s3(basename) {
     const Drive = use('Drive')
     try {
-        console.log('backup upload to drive began.')
+        log(Farbe.cyan('backup upload to drive began.'))
         let done = await Drive.disk('s3').put(backup.driverPath + basename, Helpers.tmpPath('backup/') + basename)
         if (done) {
-            console.log('backup has been uploaded sucessfully!')
+            log(Farbe.green('backup has been uploaded successfully!'))
         } else {
-            console.log('backup has not been uploaded sucessfully!')
+            log(Farbe.red('backup has not been uploaded successfully!'))
         }
     } catch (err) {
-        console.log(err)
+        log(Farbe.red(err))
     }
 }
 
 function humanSize(bytes) {
-    var sizes = ['Bytes', 'KB', 'MB', 'GB']
-    if (bytes == 0) return '0 Byte'
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
-    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (bytes === 0) return '0 Byte'
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
+    if (i === 0) return `${bytes} ${sizes[i]})`
+    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
 }
-module.exports = { runBackup }
+module.exports = { backupFactory }
