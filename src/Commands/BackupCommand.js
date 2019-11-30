207c4ff8647c61cad53af0ba6bd780704013f61c
H. Elturkey <13208801+helturkey@users.noreply.github.com>
2019-11-30 12:41:58 +0200

    Add files via upload

--- src/Commands/BackupCommand.js
+++ src/Commands/BackupCommand.js
@@ -9,22 +9,27 @@
 */
 
 const { Command } = require('@adonisjs/ace')
-const { runBackup } = require('../Backup')
+const { backupFactory } = require('../Backup')
 const Helpers = use('Helpers')
 
 class BackupCommand extends Command {
     static get signature() {
-        return 'backup:start'
+        return `
+        backup:start 
+        { --cloudOnly? : remove local file after backup to cloud.} 
+        { --localOnly? : backup file to local only.} 
+        { --restore? : restore backup file to your app, be careful, it will replace your files.}
+        `
     }
 
     static get description() {
-        return 'backup command for files.'
+        return 'a package to backup your app files.'
     }
 
-    async handle(args, options) {
+    async handle(args, flags) {
         this.info('Backup has been began')
         await this.ensureDir(Helpers.tmpPath('backup'))
-        await runBackup()
+        await backupFactory(flags)
     }
 }
 
